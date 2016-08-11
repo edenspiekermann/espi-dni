29f2f6e016e5cb0ba44909b7e52e0517ae631a47
Niall Burkley <niallburkley@gmail.com>
2016-08-11 17:47:43 +0200

    Update bot supervisor to allow dynamic bot children

--- lib/espi_dni/bot_supervisor.ex
+++ lib/espi_dni/bot_supervisor.ex
@@ -4,12 +4,19 @@ defmodule EspiDni.BotSupervisor do
   alias EspiDni.Team
   alias EspiDni.SlackRtm
 
+  # A simple module attribute that stores the supervisor name
+  @name EspiDni.BotSupervisor
+
   def start_link do
-    Supervisor.start_link(__MODULE__, :ok)
+    Supervisor.start_link(__MODULE__, :ok, name: @name)
+  end
+
+  def start_bot(token) do
+    Supervisor.start_child(@name, worker(SlackRtm, [token]))
   end
 
   def init(:ok) do
-    children = for team <- Repo.all(Team), do: worker(SlackRtm, [team.token])
+    children = for team <- Repo.all(Team), do: worker(SlackRtm, [team.token], restart: :temporary)
     supervise(children, strategy: :one_for_one)
   end
 end
