b9c416542daf56b9749e0a282bc871a95989488a
Niall Burkley <niallburkley@gmail.com>
2016-09-21 14:42:22 +0200

    Log slack activity instead of printing/sending mesages

--- lib/espi_dni/slack_rtm.ex
+++ lib/espi_dni/slack_rtm.ex
@@ -1,34 +1,28 @@
 defmodule EspiDni.SlackRtm do
   use Slack
+  require Logger
 
   @token Application.get_env(:espi_dni, __MODULE__)[:slack_token]
 
   def start_link, do: start_link(@token)
 
   def handle_connect(slack) do
-    IO.puts "Connected as #{slack.me.name}"
+    Logger.info "Connected as #{slack.me.name}"
   end
 
   def handle_message(message = %{type: "message"}, slack) do
     if message.user != slack.me.id do
-      IO.inspect(message)
-      send_message("I got a message!", message.channel, slack)
+      Logger.info "Message received: #{slack.me.name}"
     end
   end
 
-  def handle_message(message = %{type: "message"}, slack) do
-    # send_message("I got a message!", message.channel, slack)
-  end
-
   def handle_message(message, _slack) do
-    # IO.inspect(message)
+    Logger.info "Received slack activity: #{message}"
   end
 
   def handle_info({:message, text, channel}, slack) do
-    IO.puts "Sending your message, captain!"
-
+    Logger.info "Sending your message: #{text}"
     send_message(text, channel, slack)
-
     {:ok}
   end
   def handle_info(_, _), do: :ok
