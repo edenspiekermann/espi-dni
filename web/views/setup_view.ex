d0537c5c0b9061a76e9aeeb8c55c0094128f7af3
Eric Schäfer <omg@eric-schaefer.com>
2016-09-21 16:14:30 +0200

    more progress on setup page

--- web/views/setup_view.ex
+++ web/views/setup_view.ex
@@ -1,3 +1,15 @@
 defmodule EspiDni.SetupView do
   use EspiDni.Web, :view
+
+  alias EspiDni.Team
+  alias EspiDni.GoogleAnalyticsClient
+
+  def web_properties(%Team{} = team) do
+    GoogleAnalyticsClient.get_properties(team)
+    |> Enum.map(&{&1.name, &1.id})
+  end
+
+  def team_changeset(%Team{} = team) do
+    EspiDni.Team.changeset(team, %{})
+  end
 end
