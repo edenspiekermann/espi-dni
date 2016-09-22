bd3ff70c3239a4306686b145310e64586a119e11
Niall Burkley <niallburkley@gmail.com>
2016-09-22 19:53:52 +0200

    Adding google_token_expires_at to teams

--- web/models/team.ex
+++ web/models/team.ex
@@ -2,20 +2,21 @@ defmodule EspiDni.Team do
   use EspiDni.Web, :model
 
   schema "teams" do
-    field :slack_token,          :string
-    field :google_token,         :string
-    field :google_refresh_token, :string
-    field :google_property_id,   :integer
-    field :name,                 :string
-    field :url,                  :string
-    field :slack_id,             :string
-    has_many :users,             EspiDni.User
+    field :slack_token,             :string
+    field :google_token,            :string
+    field :google_refresh_token,    :string
+    field :google_property_id,      :integer
+    field :name,                    :string
+    field :url,                     :string
+    field :slack_id,                :string
+    field :google_token_expires_at, Ecto.DateTime
+    has_many :users,                EspiDni.User
 
     timestamps
   end
 
   @required_fields ~w(slack_token name slack_id)
-  @optional_fields ~w(url google_token google_refresh_token google_property_id)
+  @optional_fields ~w(url google_token google_refresh_token google_property_id google_token_expires_at)
 
   @doc """
   Creates a changeset based on the `model` and `params`.
