29a973b32a79237da59b8a9d8c02778588b61224
Niall Burkley <niallburkley@gmail.com>
2016-09-27 12:58:07 +0200

    Upgrade to elixir 1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EspiDni.Mixfile do
   def project do
     [app: :espi_dni,
      version: "0.0.1",
-     elixir: "~> 1.0",
+     elixir: "~> 1.3",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
