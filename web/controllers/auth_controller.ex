0b9adccfe38d3be7386025b617ef8a8533bd5cea
Niall Burkley <niallburkley@gmail.com>
2016-10-03 16:26:38 +0200

    Get team from assigns not session

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -26,8 +26,7 @@ defmodule EspiDni.AuthController do
       {:ok, team, user} ->
         conn
         |> put_flash(:info, "Successfully authenticated.")
-        |> put_session(:team_id, team.id)
-        |> put_session(:user_id, user.id)
+        |> assign_team_and_user(team, user)
         |> start_bot
         |> redirect(to: setup_path(conn, :index))
       {:error, reason} ->
@@ -38,13 +37,13 @@ defmodule EspiDni.AuthController do
   end
 
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, %{"provider" => "google"} = _params) do
-    team = get_session(conn, :current_team)
+    team = conn.assigns.current_team
 
     case GoogleAuthHandler.update_from_auth(team, auth) do
       {:ok, team} ->
         conn
         |> put_flash(:info, "Team updated successfully.")
-        |> put_session(:team_id, team.id)
+        |> assign(:current_team, team)
         |> redirect(to: setup_path(conn, :index))
       {:error, _changeset} ->
         conn
@@ -54,9 +53,18 @@ defmodule EspiDni.AuthController do
   end
 
   defp start_bot(conn) do
-    team = get_session(conn, :current_team)
+
+    team = conn.assigns.current_team
     EspiDni.BotSupervisor.start_bot(team.slack_token)
     conn
+  end
+
+  defp assign_team_and_user(conn, team, user) do
+    conn
+    |> put_session(:team_id, team.id)
+    |> put_session(:user_id, user.id)
+    |> assign(:current_team, team)
+    |> assign(:current_user, user)
   end
 
 end
