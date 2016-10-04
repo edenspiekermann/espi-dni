b712d675192c3651efff50878567d3fd84ce35c4
Niall Burkley <niallburkley@gmail.com>
2016-10-04 16:26:03 +0200

    Adds team analytic workers to startup task

--- lib/espi_dni/team_setup.ex
+++ lib/espi_dni/team_setup.ex
@@ -6,10 +6,32 @@ defmodule EspiDni.TeamSetup do
   import Ecto.Query, only: [from: 2]
 
   def start_link do
-    for team <- teams do
-      start_bot(team)
-    end
+    start_slack_bots
+    start_analytic_workers
     :ignore
+  end
+
+  defp start_slack_bots do
+    for team <- slack_teams, do: start_bot(team)
+  end
+
+  defp start_analytic_workers do
+    for team <- teams_with_analytics_configs, do: start_analytics(team)
+  end
+
+  defp slack_teams do
+    Repo.all(
+      from team in EspiDni.Team,
+      where: not is_nil(team.slack_token)
+    )
+  end
+
+  defp teams_with_analytics_configs do
+    Repo.all(
+      from team in EspiDni.Team,
+      where: not is_nil(team.google_token),
+      where: not is_nil(team.google_property_id)
+    )
   end
 
   defp start_bot(team) do
@@ -21,10 +43,12 @@ defmodule EspiDni.TeamSetup do
     end
   end
 
-  defp teams do
-    Repo.all(
-      from team in EspiDni.Team,
-      where: not is_nil(team.slack_token)
-    )
+  defp start_analytics(team) do
+    case EspiDni.AnalyticsSupervisor.start_anlaytics_worker(team) do
+      {:ok, _pid} ->
+        Logger.info("Successfully started anlaytic worker for team #{team.id}")
+      {:error, error} ->
+        Logger.error("Could not start anlaytic worker for team #{team.id}. Error: #{inspect error}")
+    end
   end
 end
