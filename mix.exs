27daedbe95ff77cde438104260573ce8e464fa1c
Niall Burkley <niallburkley@gmail.com>
2016-10-05 13:56:08 +0200

    Upgrading to phoenix 1.2.1 and making necessary changes
    Following upgrade guide here:
    https://gist.github.com/chrismccord/29100e16d3990469c47f851e3142f766

--- mix.exs
+++ mix.exs
@@ -44,10 +44,10 @@ defmodule EspiDni.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.1.6"},
+    [{:phoenix, "~> 1.2.1"},
      {:postgrex, ">= 0.0.0"},
-     {:phoenix_ecto, "~> 2.0"},
-     {:phoenix_html, "~> 2.4"},
+     {:phoenix_ecto, "~> 3.0"},
+     {:phoenix_html, "~> 2.7"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
      {:slack, "~> 0.7.0"},
