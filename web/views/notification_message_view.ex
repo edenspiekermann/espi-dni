05db4a4de59251229872b2f9b9b3649da74036af
Niall Burkley <niallburkley@gmail.com>
2017-02-20 17:51:46 +0100

    Add basic styles and pages for notification_messages

--- web/views/notification_message_view.ex
+++ web/views/notification_message_view.ex
@@ -2,7 +2,18 @@ defmodule EspiDni.NotificationMessageView do
   use EspiDni.Web, :view
 
   def notification_types do
-    [{"one", "foo"}, {"two", "Test"}]
+    [
+      {"View Count Spike Message", "view_count_spike"},
+      {"Source Spike Message", "source_spike"}
+    ]
+  end
+
+  def view_count_messages(messages) do
+    messages |> Enum.filter(&(&1.type == "view_count_spike"))
+  end
+
+  def source_messages(messages) do
+    messages |> Enum.filter(&(&1.type == "source_spike"))
   end
 
 end
